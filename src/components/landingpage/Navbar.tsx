13c4518d4309ea8e8d51d333ce2f7e213b70d80e
Beejayofgod <adexbolaji100@gmail.com>
2025-10-31 05:00:43 +0100

    fixed overlay issue

--- src/components/landingpage/Navbar.tsx
+++ src/components/landingpage/Navbar.tsx
@@ -3,7 +3,7 @@ import MobileNav from "./MobileNav";
 
 const Navbar = () => {
     return (
-        <header className="sticky h-20 flex items-center top-0 w-full border-b z-99  mx-auto bg-secondary ">
+        <header className="sticky h-20 flex items-center top-0 w-full border-b z-49  mx-auto bg-secondary ">
             <div className="container mx-auto  flex px-8">
                 {/* Desktop */}
                 <MainNav />
